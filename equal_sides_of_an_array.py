3158464d59679f050498647df5b3fb78c3767caf
Ivan Novikov <warhak511@mail.ru>
2023-10-30 16:55:02 +0300

    add equal_sides_of_an_array

--- equal_sides_of_an_array.py
+++ equal_sides_of_an_array.py
@@ -1,6 +1,6 @@
 # условия задачи https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/python
 
-def find_balance_index(arr):
+def find_even_index(arr):
     total_sum = sum(arr)
     left_sum = 0
 
